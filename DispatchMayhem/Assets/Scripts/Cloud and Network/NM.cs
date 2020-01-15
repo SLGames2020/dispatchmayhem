b9cd8fb492724d51e2120c0394e5f89ecb808891
SyTuck <44281354+SyTuck@users.noreply.github.com>
2020-01-15 13:41:46 -0500

    added networking manager
    NM singleton

---
+++ DispatchMayhem/Assets/Scripts/Cloud and Network/NM.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NM : MonoBehaviour
+{
+
+    private static NM instance = null;
+    public static NM inst { get { return instance; } }
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnApplicationQuit()
+    {
+        instance = null;
+    }
+}
