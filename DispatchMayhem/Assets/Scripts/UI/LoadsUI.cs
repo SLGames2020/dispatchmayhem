26dc85e79718266b7aceb5fc2a6a4eef59181bc1
Tranquil <44653127+mjes87@users.noreply.github.com>
2020-01-22 11:49:57 -0500

    Renamed UI Scripts

--- DispatchMayhem/Assets/Scripts/UI/LoadsUI.cs
+++ DispatchMayhem/Assets/Scripts/UI/LoadsUI.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Loads : MonoBehaviour
+public class LoadsUI : MonoBehaviour
 {
     public GameObject MainPanel;
     public Text titleText;
