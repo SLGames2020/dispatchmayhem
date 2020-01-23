dbe2984df8d1b3232647e7bac7a48d8b84669198
maddogmitch <mightymitch66@gmail.com>
2020-01-23 13:11:40 -0500

    Classes And Functions

--- DispatchMayhem/Assets/Scripts/DataClasses/TruckType.cs
+++ DispatchMayhem/Assets/Scripts/DataClasses/TruckType.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class TruckType
 {
+    //have no idea what to put in here maybe change this into the trucktype function - Mitchell
     public string label = "undefined truck";
 
 }
