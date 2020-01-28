be56a4a377532d666e15bca13bf1853fce97e0cc
SyTuck <jordan.tuck@sl.on.ca>
2020-01-28 01:11:52 -0500

    rudementary listbox
    Added a basic listbox. It looks like crap and still needs a click event method/listener, but it does list things

--- DispatchMayhem/Assets/Prefabs/City/City.cs
+++ DispatchMayhem/Assets/Prefabs/City/City.cs
@@ -1,59 +1,107 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class City : MonoBehaviour
 {
     public GameObject loadPrefab;
+    public GameObject listItemTemplate;
+    public GameObject loadBoxContent;
 
     public string label = "unknown city";
     public Vector2 gps;
+    public bool stillOpen = true;
 
     //[HideInInspector]
     public List<GameObject> loads;
 
-    private float lastTim;
-
+    //private float lastTime;
 
     private void Awake()
     {
         loads.Clear();
+
     }
     // Start is called before the first frame update
     void Start()
     {
         GM.inst.AddCityToMasterList(this.gameObject);
-        lastTim = Time.time + 1.0f;
+        StartCoroutine(SpawnLoad());
+        //lastTime = Time.time + 1.0f;
     }
 
     // Update is called once per frame
-    void Update()
+   /* void Update()
     {
-        if (Time.time > lastTim)
+        if (Time.time > lastTime)
         {
-            lastTim = Time.time + 1.0f;
+            lastTime = Time.time + 1.0f;
+            Debug.Log(label + " updating");
+            if (Random.Range(0, GM.inst.LOADSPAWNTIME) == 0)
+            {
+                Debug.Log(label + " spawning load");
+                SpawnLoad();
+            }
+        }
+    }*/
 
-           if (Random.Range(0, GM.inst.LOADSPAWNTIME) == 0)
-           {
-                GameObject go;
-                Vector2 des;
-                do
-                {
-                    go = GM.inst.openCities[Random.Range(0, GM.inst.openCities.Count - 1)];
-                    des = go.GetComponent<City>().gps;
-                }
-                while (des == gps);
-                
-                GameObject ldgo = Instantiate(loadPrefab, this.transform.position, Quaternion.identity);
-                Load ld = ldgo.GetComponent<Load>();
-                ld.origin = gps;
-                ld.destination = des;
-                ldgo.name = label + "load: " + loads.Count;
-                ldgo.transform.parent = this.transform;
+    /**************************************************************************
+        SpawnLoad
+
+        This method creates a load, randomly creates it's parameters, addes
+        it to the GM wrldLoads list
+
+    ****************************************************************************/
+    private IEnumerator SpawnLoad()
+    {
+        GameObject go;
+        Vector2 des;
+
+        while (stillOpen)
+        {
+            yield return new WaitForSeconds(Random.Range(1, GM.inst.LOADSPAWNTIME));
+            do
+            {
+                go = GM.inst.openCities[Random.Range(0, GM.inst.openCities.Count - 1)];
+                des = go.GetComponent<City>().gps;
+            }
+            while (des == gps);                     //loop till we find a city that isn't us
+
+            GameObject ldgo = Instantiate(loadPrefab, this.transform.position, Quaternion.identity);
+            Load ld = ldgo.GetComponent<Load>();
+            ld.originLabel = label;
+            ld.origin = gps;
+            ld.destinationLabel = go.GetComponent<City>().label;
+            ld.destination = des;
+            ldgo.name = label + "load: " + loads.Count;
+            ldgo.transform.parent = this.transform;
+            if (AddLoadToLoadBox(ld))
+            {
                 loads.Add(ldgo);
                 GM.inst.AddLoadToMasterList(ldgo);
                 Debug.Log("Load Added for " + label);
-           }
+            }
+            else
+            {
+                Destroy(ldgo);
+                Debug.Log("Failed to add load to list for " + label);
+            }
         }
     }
+
+     /****************************************************************************
+        AddLoadToLoadBox
+        
+     *****************************************************************************/
+    private bool AddLoadToLoadBox(Load ld)
+    {
+        string txt = ld.originLabel + " to " + ld.destinationLabel;
+        var copy = Instantiate(listItemTemplate);
+        copy.transform.parent = loadBoxContent.transform;
+        copy.GetComponentInChildren<Text>().text = txt;
+        return true;
+    }
+
+
 }
--- DispatchMayhem/Assets/Scripts/DataClasses/Load.cs
+++ DispatchMayhem/Assets/Scripts/DataClasses/Load.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class Load : MonoBehaviour
 {
+    public string originLabel;
     public Vector2 origin;
+    public string destinationLabel;
     public Vector2 destination;
-    public int  typeIDX;
 
+    public int typeIDX;
     public float weight = 0.0f;
     public float pay = 0.0f;
 
     public Load() { }
-    public Load(Vector2 o, Vector2 d, int t=0, float w=1000.0f, float p=1.5f)
-               { origin = o; destination = d; typeIDX = t; weight = w; pay = p; }
+    //public Load(Vector2 o, Vector2 d, int t=0, float w=1000.0f, float p=1.5f)
+    //           { origin = o; destination = d; typeIDX = t; weight = w; pay = p; }
 
 }
