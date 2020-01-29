762c8dbc4f4b75327b1bcf90fe6993737f7f0251
Amaljith Kavungalparambil Ayyappan <55007565+amaljithka@users.noreply.github.com>
2020-01-29 12:01:04 -0500

    Fixed Audio(Prototype Scene)

---
+++ demo/Assets/ClickSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ClickSound : MonoBehaviour
+{
+    public AudioSource sound;
+
+    public AudioClip click;
+    
+    public void Clicksound()
+    {
+        sound.PlayOneShot(click);
+    }
+    
+}
