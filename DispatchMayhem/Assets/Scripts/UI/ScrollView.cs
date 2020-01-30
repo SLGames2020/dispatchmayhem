2c250890e85cc3d3fcf40e164716acbd87761f14
Mike Sullivan <mjes87@gmail.com>
2020-01-30 13:51:22 -0500

    Revert "Removed unused UI scripts"
    This reverts commit d0e6f681b9538aeae22e2f016105fdcbc1ce5c08.

---
+++ DispatchMayhem/Assets/Scripts/UI/ScrollView.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollView : MonoBehaviour
+{
+    public GameObject ScrollBox;
+    int counter;
+
+    public void togglePanel()
+    {
+        counter++;
+        if (counter % 2 == 1)
+        {
+            ScrollBox.gameObject.SetActive(true);
+        }
+        else
+        {
+            ScrollBox.gameObject.SetActive(false);
+        }
+    }
+}
