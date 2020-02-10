3fcd9171dd8157451eaa87b18b82a0a3ee118853
maddogmitch <mightymitch66@gmail.com>
2020-02-09 21:33:05 -0500

    Truck/Trailers Prices

--- DispatchMayhem/Assets/Scripts/DataClasses/TruckType.cs
+++ DispatchMayhem/Assets/Scripts/DataClasses/TruckType.cs
@@ -23,6 +23,29 @@ public class TruckType : MonoBehaviour
         maxFuel = maxF;
     }
 
+    /* public void SemiTrucks()
+     {
+         TruckType[] SemiTruck =
+         {
+             new SemiTruck1(0, 500, 0, 0, 115, 125000.00f, 500.00f);
+             new SemiTruck2(0, 550, 0, 0, 115, 150000.00f, 600.00f);
+             new SemiTruck3(0, 600, 0, 0, 115, 175000.00f, 700.00f);
+             new SemiTruck4(0, 650, 0, 0, 115, 200000.00f, 800.00f);
+             new SemiTruck5(0, 700, 0, 0, 115, 225000.00f, 900.00f);
+             new SemiTruck6(0, 750, 0, 0, 115, 250000.00f, 1000.00f);
+         }
+     }*/
+
+    /*public void BoxTruck()
+    {
+            new Boxtruck1(0, 200, 0, 200, 70, 25000.00f, 100.00f);
+            new BoxTruck2(0, 250, 0, 250, 80, 50000.00f, 200.00f);
+            new BoxTruck3(0, 300, 0, 300, 90, 75000.00f, 300.00f);
+            new BoxTruck4(0, 350, 0, 350, 100, 100000.00f, 400.00f);
+            new BoxTruck5(0, 400, 0, 400, 110, 125000.00f, 450.00f);
+            new BoxTruck6(0, 450, 0, 450, 120, 150000.00f, 500.00f);
+    }*/
+
     public void UsedTruck()
     {
         if (age >= 5)
