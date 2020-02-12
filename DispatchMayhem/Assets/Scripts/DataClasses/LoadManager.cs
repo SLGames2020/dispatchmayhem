0976c1a12573280456f1cc74a7a5993661c5bae9
maddogmitch <mightymitch66@gmail.com>
2020-02-12 11:07:47 -0500

    Started Load Managements

---
+++ DispatchMayhem/Assets/Scripts/DataClasses/LoadManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadManager : MonoBehaviour
+{
+    
+
+    public string originLabel;
+    public Vector2 origin;
+    public string destinationLabel;
+    public Vector2 destination;
+
+    public float pay;
+    public float space;
+
+    public void AllowedLoads()
+    {
+
+    }
+
+    public void LoadChecker()
+    {
+
+    }
+
+    
+
+}
