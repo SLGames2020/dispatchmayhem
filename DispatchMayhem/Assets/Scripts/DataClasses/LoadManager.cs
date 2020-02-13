83591b120a5c8eba5cdd5409c8fb1cc387d9ab70
Mitchell Labrecque [Student] <mitchell.labrecque@student.sl.on.ca>
2020-02-13 13:37:49 -0500

    Some Product PseudoCode

--- DispatchMayhem/Assets/Scripts/DataClasses/LoadManager.cs
+++ DispatchMayhem/Assets/Scripts/DataClasses/LoadManager.cs
@@ -11,17 +11,54 @@ public class LoadManager : MonoBehaviour
     public string destinationLabel;
     public Vector2 destination;
 
+    public GameObject trailer;
+
     public float pay;
     public float space;
 
+    public enum Products
+    {
+        BOXES,
+        COLDGOODS,
+        CONTRUCTION_LARGE,
+        CONTRUCTION,
+        LIQUIDS,
+    }
+
+    public Products allowed;
+
     public void AllowedLoads()
     {
+        trailer.GetComponent<Trailer>();
+        if(allowed == Products.BOXES)
+        {
 
+        }
+        else if(allowed == Products.COLDGOODS)
+        {
+
+        }
+        else if(allowed == Products.CONTRUCTION)
+        {
+
+        }
+        else if(allowed == Products.CONTRUCTION_LARGE)
+        {
+
+        }
+        else if(allowed == Products.LIQUIDS)
+        {
+
+        }
     }
 
     public void LoadChecker()
     {
-
+        bool isAllowed = true;
+        /*if(allowed == Products.LIQUIDS && Trailer.TrailerType != Trailer.TrailerType.TANKER)
+        {
+            isAllowed = false;
+        }*/
     }
 
     
