d7745c9181ea4310bac29b7821e328fe1618100a
JesseRobert Wilband [Student] <jesserobert.wilband@student.sl.on.ca>
2020-03-02 17:03:29 -0500

    add Map Manager

---
+++ DispatchMayhem/Assets/MapM.cs
@@ -0,0 +1,132 @@
+﻿namespace Mapbox.Examples
+{
+
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using Mapbox.Utils;
+    using Mapbox.Unity.Map;
+    using Mapbox.Unity.MeshGeneration.Factories;
+    using Mapbox.Unity.Utilities;
+
+
+    public class MapM : MonoBehaviour
+    {
+        private static MapM instance = null;
+        public static MapM inst { get { return instance; } }
+
+        [SerializeField]
+        AbstractMap _map;
+
+        [SerializeField]
+        [Geocode]
+        string[] _locationStrings;
+        Vector2d[] _locations;
+
+        [SerializeField]
+        float _spawnScale = 3.0f;
+
+        //[SerializeField]
+        //GameObject _truckPrefab;
+        //[SerializeField]
+        //GameObject _trailerPrefab;
+
+        List<GameObject> _spawnedObjects;
+
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else if (instance != this)
+            {
+                Destroy(this.gameObject);
+            }
+
+            _spawnedObjects = new List<GameObject>();
+            _spawnedObjects.Clear();
+        }
+
+        private void OnApplicationQuit()
+        {
+            instance = null;
+        }
+
+        void Start()
+        {
+            //_map = this.gameObject.GetComponent<AbstractMap>();
+            _locations = new Vector2d[_locationStrings.Length];
+
+            for (int i = 0; i < _locationStrings.Length; i++)
+            {
+                var locationString = _locationStrings[i];
+                _locations[i] = Conversions.StringToLatLon(locationString);
+            }
+        }
+
+        private void Update()
+        {
+            int count = _spawnedObjects.Count;
+            for (int i = 0; i < count; i++)
+            {
+                var spawnedObject = _spawnedObjects[i];
+                //var location = _locations[i];
+                Vector2d loc = Vec2To2d(spawnedObject.GetComponent<MapSupport>().gps);
+                //spawnedObject.transform.localPosition = _map.GeoToWorldPosition(location, true);
+                Vector2d tloc = new Vector2d(loc.y, loc.x);
+                spawnedObject.transform.localPosition = _map.GeoToWorldPosition(tloc, true);
+                spawnedObject.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+                //Debug.Log(spawnedObject.transform.localPosition);
+            }
+        }
+
+        /******************************************************************************************
+                AddToMap
+
+                This method takes a game object and adds it to map. Not that the game object must 
+                have an attached MapSupport script
+
+         *******************************************************************************************/
+        public bool AddToMap(GameObject go)
+        {
+            MapSupport ms = go.GetComponent<MapSupport>();
+            bool retval = true;
+
+            if (ms == null)
+            {
+                Debug.Log("Could not add " + go.name + " to the map");
+                retval = false;
+            }
+            else
+            {
+                _spawnedObjects.Add(go);
+                Debug.Log("Added to Map! " + ms.gps);
+                Debug.Log("Could not add " + go.name + " to the map");
+                go.transform.localPosition = _map.GeoToWorldPosition(Vec2To2d(ms.gps), true);
+                go.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
+                //Debug.Log("Added to Map! " + ms.gps);
+            }
+
+            return retval;
+
+        }
+
+        /******************************************************************************************
+            Vec2To2d
+
+            This method takes a standard unity Vector2 (Unity v2017 and newer) to a Vector2d
+            (unity v5)
+
+        *******************************************************************************************/
+        public Vector2d Vec2To2d(Vector2 vec2)
+        {
+            Vector2d tv2d;
+            tv2d.x = vec2.x;
+            tv2d.y = vec2.y;
+            return (tv2d);
+        }
+    }
+}
+
+    
--- DispatchMayhem/Assets/Scripts/MapSupport.cs
+++ DispatchMayhem/Assets/Scripts/MapSupport.cs
@@ -7,13 +7,13 @@ using Mapbox.Examples;
 public class MapSupport : MonoBehaviour
 {
     public Vector2 gps;
-    private Mapbox.Examples.SpawnToMap spawntomap;
+    //private Mapbox.Examples.SpawnToMap spawntomap;
     
 
     void Awake()
     {
-        GameObject mapOB = GameObject.Find("Map");
-        spawntomap = mapOB.GetComponent<SpawnToMap>();
+        //GameObject mapOB = MapM.inst._map;
+        //spawntomap = mapOB.GetComponent<SpawnToMap>();
         gps.y = 45.016667f;                             //default all objects to cornwall for now
         gps.x = -74.733333f;                            //(this will need to be changed to a "Depot" lookup eventually)
 
@@ -22,7 +22,8 @@ public class MapSupport : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        spawntomap.AddToMap(this.gameObject);
+        //spawntomap.AddToMap(this.gameObject);
+        MapM.inst.AddToMap(this.gameObject);
     }
 
     // Update is called once per frame
