47118990276ab7d490378c8cdb08cca6f12773da
RyanMassia <ryanmassia6@gmail.com>
2020-03-07 16:03:00 -0500

    test

--- DispatchMayhem/Assets/Scripts/DataClasses/Truck.cs
+++ DispatchMayhem/Assets/Scripts/DataClasses/Truck.cs
@@ -6,8 +6,7 @@ public class Truck : MonoBehaviour
 {
     public enum TruckStates { IDLE, LOADING, ONROUTE, UNLOADING, IMPOUNDED, REPAIRING }
 
-    // public LoadType type;
-    public LoadManager.Products type;
+    //public LoadType type; // this is breaking the build... we don't have a loadtype class...
     public string rigLabel = "Undefined transport";
 
     public TruckStates status = TruckStates.IDLE;
