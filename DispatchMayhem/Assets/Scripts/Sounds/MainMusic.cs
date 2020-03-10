661418871d9aeb47b6960e4d721bf6a7df81949a
maddogmitch <mightymitch66@gmail.com>
2020-03-10 11:46:34 -0400

    Added working Non Annoying music

---
+++ DispatchMayhem/Assets/Scripts/Sounds/MainMusic.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMusic : MonoBehaviour
+{
+
+    public AudioClip mainMusic;
+
+    void Start()
+    {
+        SoundManager.instance.MainMusic(mainMusic);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
