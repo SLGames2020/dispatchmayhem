9b98802f776ed81d7f92c3fcc311f9f44f522f3e
maddogmitch <mightymitch66@gmail.com>
2020-03-10 10:55:11 -0400

    New SoundManager

--- DispatchMayhem/Assets/Scripts/Sounds/SoundManager.cs
+++ DispatchMayhem/Assets/Scripts/Sounds/SoundManager.cs
@@ -4,41 +4,36 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
-    public AudioClip mainMusic;
-    public AudioClip truckArrives;
-    public AudioClip truckLeaves;
-    public AudioClip scaleStation;
-    public AudioClip issueOccurs;
-    public AudioClip fined;
-    public AudioClip newLoad;
-    public AudioClip buttonClicks;
-    public static SoundManager instance = null;
-    private AudioSource soundEffectAudio;
+    public AudioSource mainMusic;
+    public AudioSource soundEffects;
 
-    private void Start()
+    public static SoundManager instance = null;
+   
+    private void Awake()
     {
 
        if (instance == null)
-        {
+       {
             instance = this;
-        }
-        else if (instance != this)
-        {
+       }
+       else if (instance != this)
+       {
             Destroy(gameObject);
-        }
-        AudioSource[] sources = GetComponents<AudioSource>();
-        foreach(AudioSource source in sources)
-        {
-            if(source.clip == null)
-            {
-                soundEffectAudio = source;
-            }
-        }
+       }
 
-        DontDestroyOnLoad(gameObject);
+       DontDestroyOnLoad(gameObject);
     }
-    public void PlayOneShot(AudioClip clip)
+   
+    public void MainMusic(AudioClip clip)
     {
-        soundEffectAudio.PlayOneShot(clip);
+        mainMusic.clip = clip;
+        mainMusic.Play();
+
+    }
+
+    public void SoundEffects(AudioClip clip)
+    {
+        soundEffects.clip = clip;
+        soundEffects.PlayOneShot(clip, 0.7f);
     }
 }
