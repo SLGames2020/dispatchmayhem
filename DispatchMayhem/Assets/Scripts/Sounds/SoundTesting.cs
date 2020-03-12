77317b9888bb32ec3a01b6f10d7dcb52e76db5f3
maddogmitch <mightymitch66@gmail.com>
2020-03-12 13:40:03 -0400

    Added the main music (for real this time!)

---
+++ DispatchMayhem/Assets/Scripts/Sounds/SoundTesting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTesting : MonoBehaviour
+{
+    public AudioClip test1;
+
+    void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            SoundManager.instance.Shop(test1);
+        }
+    }
+}
