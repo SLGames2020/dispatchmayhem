a557fe7fcfc2fec71b0e3d813532a8157f25d5e2
Jesse <jwilband2017@gmail.com>
2020-03-16 20:40:46 -0400

    added zoom controls to mouse scroll

--- DispatchMayhem/Assets/Scripts/MapM.cs
+++ DispatchMayhem/Assets/Scripts/MapM.cs
@@ -1,6 +1,6 @@
 ﻿namespace Mapbox.Examples
-{
 
+{
     using System.Collections;
     using System.Collections.Generic;
     using UnityEngine;
@@ -26,11 +26,6 @@
         [SerializeField]
         float _spawnScale = 3.0f;
 
-        //[SerializeField]
-        //GameObject _truckPrefab;
-        //[SerializeField]
-        //GameObject _trailerPrefab;
-
         List<GameObject> _spawnedObjects;
 
         void Awake()
@@ -55,7 +50,6 @@
 
         void Start()
         {
-            //_map = this.gameObject.GetComponent<AbstractMap>();
             _locations = new Vector2d[_locationStrings.Length];
 
             for (int i = 0; i < _locationStrings.Length; i++)
@@ -73,13 +67,33 @@
                 var spawnedObject = _spawnedObjects[i];
                 MapSupport msup = spawnedObject.GetComponent<MapSupport>();
                 Vector2d loc = Vec2To2d(msup.gps);
-                if (spawnedObject.tag == "City") loc = loc + new Vector2d(-3.20f, 0.15f);                   //shift over the cities because they are positioned improperly for some reason
+                if (spawnedObject.tag == "City") loc = loc + new Vector2d(-3.20f, 0.15f); //shift over the cities because they are positioned improperly for some reason
 
                 Vector2d tloc = new Vector2d(loc.y, loc.x);
                 spawnedObject.transform.localPosition = _map.GeoToWorldPosition(tloc, true);
-                //spawnedObject.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
                 spawnedObject.transform.localScale = new Vector3(msup.baseScale.x*_map.Zoom, msup.baseScale.y*_map.Zoom, msup.baseScale.z *_map.Zoom);
-                //Debug.Log("base: " + msup.baseScale + " Zoom: " + _map.Zoom);
+            }
+
+            //---------------------------------------------------------------------------------//
+            //                               MAP CONTROLS                                      //
+            //---------------------------------------------------------------------------------//
+
+            if (Input.GetAxis("Mouse ScrollWheel") > 0.0f) // zoom in
+            {
+                _map.UpdateMap(_map.AbsoluteZoom + 1.0f);
+                if (_map.AbsoluteZoom >= 12.0f)
+                {
+                    _map.UpdateMap(12.0f);
+                }
+            }
+
+            if (Input.GetAxis("Mouse ScrollWheel") < 0.0f) // zoom out
+            {
+                _map.UpdateMap(_map.AbsoluteZoom - 1.0f);
+                if (_map.AbsoluteZoom <= 8.0f)
+                {
+                    _map.UpdateMap(8.0f);
+                }
             }
         }
 
@@ -88,7 +102,6 @@
 
                 This method takes a game object and adds it to map. Not that the game object must 
                 have an attached MapSupport script
-
          *******************************************************************************************/
         public bool AddToMap(GameObject go)
         {
@@ -103,14 +116,11 @@
             else
             {
                 _spawnedObjects.Add(go);
-               // Debug.Log("Added to Map! " + ms.gps);
-               // Debug.Log(go.name + " added to the map");
                 go.transform.localPosition = _map.GeoToWorldPosition(Vec2To2d(ms.gps), true);
                 go.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
             }
 
             return retval;
-
         }
 
         /******************************************************************************************
@@ -118,7 +128,6 @@
 
             This method takes a standard unity Vector2 (Unity v2017 and newer) to a Vector2d
             (unity v5)
-
         *******************************************************************************************/
         public Vector2d Vec2To2d(Vector2 vec2)
         {
