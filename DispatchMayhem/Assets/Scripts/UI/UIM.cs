c026acbf2bf6fb6e24f1be5031424c776a28a52f
RyanMassia <ryanmassia6@gmail.com>
2020-03-26 19:17:39 -0400

    Trailer window populated and buttons set up

--- DispatchMayhem/Assets/Scripts/UI/UIM.cs
+++ DispatchMayhem/Assets/Scripts/UI/UIM.cs
@@ -16,6 +16,9 @@ public class UIM : MonoBehaviour
     public GameObject TrucksPanel;
     public GameObject JobsPanel;
     public GameObject TruckerPanel;
+    public GameObject TruckPanel;
+    public GameObject TrailerPanel;
+    public GameObject ConfirmPanel;
 
     public GameObject[] Trucks;
 
@@ -52,6 +55,9 @@ public class UIM : MonoBehaviour
         TrucksPanel.SetActive(false);
         JobsPanel.SetActive(false);
         TruckerPanel.SetActive(false);
+        TruckPanel.SetActive(false);
+        TrailerPanel.SetActive(false);
+        ConfirmPanel.SetActive(false);
     }
 
     public void exitGame()
