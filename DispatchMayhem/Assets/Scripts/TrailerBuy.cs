7cc7d6b3266d3bf811ab7bd214aaa1c933943c59
RyanMassia <ryanmassia6@gmail.com>
2020-03-26 21:06:44 -0400

    Purchase Trailer and Truck now work and subtract money from player

---
+++ DispatchMayhem/Assets/Scripts/TrailerBuy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailerBuy : MonoBehaviour
+{
+
+    public void BuyNewTanker()
+    {
+        Finances.inst.ValidatePurchase(40000.0f);
+    }
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+        
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+        
+    //}
+}
