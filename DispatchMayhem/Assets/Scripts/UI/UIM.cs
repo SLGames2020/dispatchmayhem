84428b802adec732a20d65cb337eb7ca95961faf
maddogmitch <mightymitch66@gmail.com>
2020-03-28 19:17:08 -0400

    Added soundsnaps and Setting editing script

--- DispatchMayhem/Assets/Scripts/UI/UIM.cs
+++ DispatchMayhem/Assets/Scripts/UI/UIM.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 
 public class UIM : MonoBehaviour
 {
@@ -17,6 +18,9 @@ public class UIM : MonoBehaviour
     public GameObject JobsPanel;
     public GameObject TruckerPanel;
     public AudioClip buttonClick;
+
+    public AudioMixerSnapshot paused;
+    public AudioMixerSnapshot unpaused;
 
     public GameObject[] Trucks;
 
@@ -42,20 +46,33 @@ public class UIM : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Escape))
         {
             SettingsPanel.SetActive(true);
-            Pause();
+            //Pause();
         }
     }
 
     public void Pause()
     {
         Time.timeScale = Time.timeScale == 0 ? 1 : 0;
+        LowPass();
+    }
+
+    public void LowPass()
+    {
+        if(Time.timeScale == 0)
+        {
+            paused.TransitionTo(0.1f);
+        }
+        else
+        {
+            unpaused.TransitionTo(0.1f);
+        }
     }
 
     public void LoadPanel(GameObject Panel)
     {
         ClosePanel();
         Panel.SetActive(true);
-        Pause();
+        //Pause();
     }
 
     public void ClosePanel()
