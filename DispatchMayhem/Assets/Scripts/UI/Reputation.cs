12aa43db8f2e2aecb857f76d1b36dc394557b7fb
maddogmitch <mightymitch66@gmail.com>
2020-04-16 14:10:56 -0400

    added reputation script

---
+++ DispatchMayhem/Assets/Scripts/UI/Reputation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Reputation : MonoBehaviour
+{
+    public Image Heart;
+
+
+    void Rep()
+    {
+        Movement loadjr = GM.inst.GetComponent<Movement>();
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            float movement = 1f;
+            float horizontalInput = Input.GetAxis("Horizontal");
+
+            Heart.transform.position = transform.position + new Vector3(horizontalInput * movement, 0);
+        }
+       
+        
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rep();
+    }
+}
